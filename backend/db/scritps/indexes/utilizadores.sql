d28c44ba9a3b29d2e3b9656c401d4b2efd3f448f
Tiago <tiago@beetle>
2025-05-14 10:54:22 +0100

    organized stuff

--- backend/db/scritps/indexes/utilizadores.sql
+++ backend/db/scritps/indexes/utilizadores.sql
@@ -1,3 +1,15 @@
+-- Index: indexNomeUt
+
+-- DROP INDEX IF EXISTS public."indexNomeUt";
+
+CREATE INDEX IF NOT EXISTS "indexNomeUt"
+    ON public.utilizadores USING btree
+    (nome COLLATE pg_catalog."default" ASC NULLS LAST)
+    INCLUDE(idutilizador, nome, ativo)
+    WITH (deduplicate_items=True)
+    TABLESPACE pg_default;
+
+
 -- Index: indexEmailUt
 
 -- DROP INDEX IF EXISTS public."indexEmailUt";
