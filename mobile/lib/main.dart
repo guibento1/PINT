34f9449d8b8afc6c233432c351b382887846a354
Guilherme Bento <bentoaguilherme@gmail.com>
2025-07-09 22:38:45 +0100

    Update main.dart

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'Projeto Prático',
+      title: 'Projeto Prático PDM',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
