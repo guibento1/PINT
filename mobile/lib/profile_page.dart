caf0211e2dc352d4a7b7a7bb3fa922fb7bd57e73
Guilherme Bento <bentoaguilherme@gmail.com>
2025-07-09 17:54:18 +0100

    mais alteracoes

--- mobile/lib/profile_page.dart
+++ mobile/lib/profile_page.dart
@@ -1,3 +1,13 @@
+import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
 
